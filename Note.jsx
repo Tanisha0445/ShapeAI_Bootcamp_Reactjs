72e29be62c447f11e02ffb1136fae068be63f289
Tanisha0445 <84021343+Tanisha0445@users.noreply.github.com>
2021-06-05 21:52:22 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1> JavaScript and React.js</h1>
+      <p>
+        Hello my name is Tanisha Shukla and i was from ShapeAi React js bootcamp, that was amazing journey that i had and learned alot from our instructor  Mr.Shaurya Sinha sir  and we covered
+        everything from the Scratch including JavaScript , React.js and HTML . 
+      </p>
+    </div>
+  );
+}
+export default Note;
